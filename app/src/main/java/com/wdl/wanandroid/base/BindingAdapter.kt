6ff76f3a941966451f5b9aa220c1563b06d4a3be
wdl <wdl@qq.com>
2020-04-13 18:05:58 +0800

    1.update banner

--- app/src/main/java/com/wdl/wanandroid/base/BindingAdapter.kt
+++ app/src/main/java/com/wdl/wanandroid/base/BindingAdapter.kt
@@ -1,11 +1,19 @@
 package com.wdl.wanandroid.base
 
 import androidx.databinding.BindingAdapter
+import com.wdl.wanandroid.adapter.ImgBannerAdapter
 import com.wdl.wanandroid.bean.BannerData
 import com.youth.banner.Banner
-import com.youth.banner.adapter.BannerAdapter
-import com.youth.banner.listener.OnBannerListener
 
 /**
  * Create by: wdl at 2020/4/13 16:57
  */
+
+
+@BindingAdapter(value = ["data"])
+fun setBannerData(
+    banner: Banner<BannerData, ImgBannerAdapter>,
+    data: List<BannerData>
+) {
+    data.let { banner.setDatas(it) }
+}
